f426cb6c655b06f379218e515f80ef086339150d
moishawa <jpdias1999z@gmail.com>
2021-02-18 07:08:24 -0300

    Aula 07 - 18/02

--- src/components/commons/Button/index.js
+++ src/components/commons/Button/index.js
@@ -2,6 +2,7 @@ import styled, { css } from 'styled-components';
 import get from 'lodash/get';
 import { TextStyleVariantsMap } from '../../foundation/Text';
 import { breakpointsMedia } from '../../../theme/utils/breakpointsMedia';
+import { propToStyle } from '../../../theme/utils/propToStyle';
 
 const ButtonGhost = css`
   color: ${({ theme, variant }) => get(theme, `colors.${variant}.color`)};
@@ -30,6 +31,8 @@ export const Button = styled.button`
       ${TextStyleVariantsMap.paragraph1}
     `,
   })}
+  ${propToStyle('margin')}
+  ${propToStyle('display')}
   ${({ ghost }) => (ghost ? ButtonGhost : ButtonDefault)}
   &:hover,
   &:focus {
--- src/components/foundation/Text/index.js
+++ src/components/foundation/Text/index.js
@@ -1,23 +1,52 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
 import PropTypes from 'prop-types';
+import { propToStyle } from '../../../theme/utils/propToStyle';
+import { breakpointsMedia } from '../../../theme/utils/breakpointsMedia';
+import get from 'lodash/get';
 
-export const TextStyleVariantsMap ={
-  paragraph1: css`
-    font-size: ${({ theme }) => theme.typographyVariants.paragraph1.fontSize};
-    font-weight: ${({ theme }) => theme.typographyVariants.paragraph1.fontWeight};
-    line-height: ${({ theme }) => theme.typographyVariants.paragraph1.lineHeight};
-  `,
 
-  smallestException: css`
-      font-size: ${({ theme }) => theme.typographyVariants.smallestException.fontSize};
-      font-weight: ${({ theme }) => theme.typographyVariants.smallestException.fontWeight};
-      line-height: ${({ theme }) => theme.typographyVariants.smallestException.lineHeight};
+const paragraph1 = css`
+  ${({ theme }) => css`
+    font-size: ${theme.typographyVariants.paragraph1.fontSize};
+    font-weight: ${theme.typographyVariants.paragraph1.fontWeight};
+    line-height: ${theme.typographyVariants.paragraph1.lineHeight};
+  `}
+`;
+
+const smallestException = css`
+  ${({ theme }) => css`
+    font-size: ${theme.typographyVariants.smallestException.fontSize};
+    font-weight: ${theme.typographyVariants.smallestException.fontWeight};
+    line-height: ${theme.typographyVariants.smallestException.lineHeight};
+  `}
+`;
+
+export const TextStyleVariantsMap = {
+  smallestException,
+  paragraph1,
+  title: css`
+    ${({ theme }) => css`
+      font-size: ${theme.typographyVariants.titleXS.fontSize};
+      font-weight: ${theme.typographyVariants.titleXS.fontWeight};
+      line-height: ${theme.typographyVariants.titleXS.lineHeight};
+    `}
+    ${breakpointsMedia({
+      md: css`
+        ${({ theme }) => css`
+          font-size: ${theme.typographyVariants.title.fontSize};
+          font-weight: ${theme.typographyVariants.title.fontWeight};
+          line-height: ${theme.typographyVariants.title.lineHeight};
+        `}
+      `,
+    })}
   `,
-}
+};
 
 const TextBase = styled.span`
-  ${(props) => TextStyleVariantsMap[props.variant]}
+  ${({ variant }) => TextStyleVariantsMap[variant]}
+  color: ${({ theme, color }) => get(theme, `colors.${color}.color`)};
+  ${propToStyle('textAlign')}
 `;
 
 export function Text({
@@ -46,5 +75,5 @@ Text.defaultProps = {
 Text.propTypes = {
   children: PropTypes.node.isRequired,
   tag: PropTypes.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'p', 'li', 'a', 'span']),
-  variant: PropTypes.oneOf(['paragraph1', 'smallestException']),
+  variant: PropTypes.oneOf(['title', 'paragraph1', 'smallestException']),
 };
