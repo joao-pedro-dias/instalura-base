ef8f8d949ea5839d050c265efc6f3267c308c0a1
moishawa <jpdias1999z@gmail.com>
2021-02-28 14:12:04 -0300

    commit-teste2

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Footer from '../src/components/commons/Footer';
+import Footer from '../src/components/commons/Footer'
 import Menu from '../src/components/commons/Menu';
 import { Text } from '../src/components/foundation/Text';
 import { Button } from '../src/components/commons/Button';
