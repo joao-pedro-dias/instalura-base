fc13ca57cb168ead5856f3bd391a70a494f9bf9b
moishawa <jpdias1999z@gmail.com>
2021-02-28 14:10:26 -0300

    commit-teste

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import Footer from '../src/components/commons/Footer';
 import Menu from '../src/components/commons/Menu';
 import { Text } from '../src/components/foundation/Text';
